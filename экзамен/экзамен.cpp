6b95627b6c59e3f4fc663975ddf45513f542d729
Andrey359 <at.tehnol@outlook.com>
2023-07-21 20:08:17 +0300

    Готово!

--- экзамен/экзамен.cpp
+++ экзамен/экзамен.cpp
@@ -1,24 +1,57 @@
 ﻿#include <iostream>
 #include <time.h>
-#include <vector>
 
 using namespace std;
 
 class AppUI {
 public:
-	int len;
+	long long len, a, b; 
+	bool l = 0;
 	void getLen() {
-		cout << "Введите длину массива: ";
-		cin >> this->len;
+		while (true)
+		{
+			if (l) {
+				len = rand() % 100 + 1;
+				break;
+			}
+
+			else {
+				cout << "Введите длину массива: ";
+				cin >> len;
+
+				if (len > 0) {
+					break;
+				}
+				if (len == 0) {
+					cerr << "Вы ввели нулевое значение, повторите попытку .." << endl;
+				}
+				if (len < 0) {
+					cerr << "Вы ввели отрицательное значение, повторите попытку .." << endl;
+				}
+				if (len > INT_MAX) {
+					cerr << "Вы ввели слишком большое значение, повторите попытку .." << endl;
+				}
+			}
+		}
 	}
 
-	template<typename T>
-	void getAB(T a, T b) {
-		cout << "Введите начало случайных чисел: ";
-		cin >> a;
+	void getAB() {
+		if (l) {
+			a = rand() % 100 + 1;
+			b = rand() % 100 + 1;
+		}
+		else {
+			cout << "Введите начало случайных чисел: ";
+			cin >> a;
 
-		cout << "Введите конец случайных чисел: ";
-		cin >> b;
+			cout << "Введите конец случайных чисел: ";
+			cin >> b;
+		}
+		if (a > b) {
+			int c = a;
+			a = b;
+			b = c;
+		}
 	}
 
 	void show() {
@@ -26,26 +59,30 @@ public:
 			cout << "целчисленный - 0\n";
 			cout << "дробный  от 0.0f до 1.0f - 1\n";
 			cout << "дробный  - 2\n";
-			cout << "Выберите режим работы: ";
+			cout << "я не знаю  - любое число\n" << endl;
+			cout << "Выберите режим работы: " << endl;;
 			cin >> mod;
+			if (mod > 2) {
+				mod = rand() % 3;
+				l = 1;
+			}
 			switch (mod)
 			{
 				case 0: 
 				{
 					getLen();
 					int* arr = new int[len];
-
-					int a, b;
-					getAB<int>(a, b);
+					getAB();
 
 					for (int i = 0; i < len; i++)
 					{
 						arr[i] = rand() % (b - a + 1) + a;
-						cout << arr[i] << endl;
+						cout <<"[" << i << "] = " << arr[i] << endl;
 					}
 					delete[] arr;
 					break;
 				}
+				
 				case 1: 
 				{
 					getLen();
@@ -53,46 +90,35 @@ public:
 
 					for (int i = 0; i < len; i++)
 					{
-						arr[i] = rand() / (float)RAND_MAX;
-						cout << arr[i] << endl;
+						arr[i] = rand() / (double)RAND_MAX;
+						cout << "[" << i << "] = " << arr[i] << endl;
 					}
 					delete[] arr;
 					break;
 				}
-				default: 
+				case 2: 
 				{
+					getLen();
 					double* arr = new double[len];
-
+					getAB();
 					for (int i = 0; i < len; i++)
 					{
-						arr[i] = (rand() / (float)RAND_MAX) * (a - b + 1) + a;
-						cout << arr[i] << endl;
+						arr[i] = (rand() / (double)RAND_MAX) * (b - a + 1) + a;
+						cout << "[" << i << "] = " << arr[i] << endl;
 					}
 					delete[] arr;
-
 					break; 
 				}
 			}
-
 		}
 	};
 
 int main()
 {
+	setlocale(LC_ALL, "");
 	srand(time(0));
+
 	AppUI a;
-
-	AppUI b;
-	//a.random();
-
-
+	a.show();
+	return 0;
 }
-
-
-/*
- Настраиваемый генератор массивов случайных чисел. Требуются
-возможности:
-a. Настройка диапазона (от а до б).
-b. Выбор длины массива.
-c. Режим дробных чисел.
-*/
